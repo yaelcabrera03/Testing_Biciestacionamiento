0e53d8eae89f57652574d0db1d3b759770505e7a
Eduardo <salinas.eduardo17a@gmail.com>
2026-05-26 16:51:30 -0600

    Archivo inicial

---
+++ test_carga.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time;
+#A - Preparar
+driver = webdriver.Chrome()
+
+
+#A - Ejecutar
+driver.get("https://yaelcabrera03.github.io/IS2026-2-Sistema-de-biciestacionamiento-/index.html")
+time.sleep(10)
+
+#A - verificar
+
+assert "Login" in driver.title
+print("El sistema cargó correctamente")
+print("Titulo: ".driver.title)
+
+# limpiar
+driver.quit()
+
