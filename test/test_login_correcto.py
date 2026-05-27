9ac9ca2cf6e4fa8bfd8876c30741f0d79b1383ce
Eduardo <salinas.eduardo17a@gmail.com>
2026-05-26 19:10:42 -0600

    estructura de carpetas y archivos iniciales

--- test/test_login_correcto.py
+++ test/test_login_correcto.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import Keys
+
 import time;
 #A - Preparar
 driver = webdriver.Chrome()
@@ -9,18 +9,17 @@ driver.get("https://yaelcabrera03.github.io/IS2026-2-Sistema-de-biciestacionamie
 
 #A - Ejecutar
 campos = driver.find_elements(By.CLASS_NAME, "entradaTexto")
-
-#campos[0].send_keys("")
-#campos[1].send_keys("")
+campos[0].send_keys("estudiante@gmail.com")
+campos[1].send_keys("123456")
 driver.find_element(By.ID,"btnIngresar").click();
 
 time.sleep(10)
 
 #A - verificar
 
-assert "Login" in driver.title
+assert "principal.html" in driver.current_url
 print("El sistema cargó correctamente")
-print("Titulo: ".driver.title)
+
 
 # limpiar
 driver.quit()
